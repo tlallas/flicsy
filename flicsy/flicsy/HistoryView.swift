7c7acb94c37987ed18ef57204de314f4250e6663
Clara <cleverett506@gmail.com>
2022-05-01 22:46:51 -0700

    Add date information to history page

--- flicsy/flicsy/HistoryView.swift
+++ flicsy/flicsy/HistoryView.swift
@@ -15,10 +15,12 @@ struct HistoryView: View {
     
     
     var body: some View {
+        
         VStack {
             List {
+                
                 ForEach(reflections, id: \.self) { reflection in
-                    NavigationLink(destination: ReflectionHistoryView(dailyImage:reflection.image!,title: reflection.title ?? "Untitled",  reflection:reflection.text ?? "no text in the reflection")) {
+                    NavigationLink(destination: ReflectionHistoryView(dailyImage:reflection.image!,title: reflection.title ?? "Untitled",  reflection:reflection.text ?? "no text in the reflection",locality:reflection.locality ?? "",date:reflection.date ?? Date(),country:reflection.country ?? "")) {
                     
                         HStack{
                          if let thumbnailImage = UIImage(data: reflection.thumbnail!){
--- flicsy/flicsy/ReflectionHistoryView.swift
+++ flicsy/flicsy/ReflectionHistoryView.swift
@@ -5,4 +5,36 @@
 //  Created by Clara Everett on 4/30/22.
 //
 
-import Foundation
+import SwiftUI
+
+struct ReflectionHistoryView: View {
+    @State var dailyImage:Data
+    @State var title:String
+    @State var reflection:String
+    @State var locality:String
+    @State var date:Date
+    @State var country:String
+    
+
+    var body: some View {
+        VStack {
+            Text("\(title)")
+                .font(.headline)
+            if let img = UIImage(data:dailyImage){
+                Image(uiImage:img).resizable().scaledToFill()
+            }
+            Text("\(date)")
+                .font(.headline)
+            Text("\(country)")
+                .font(.headline)
+            Text("\(reflection)")
+                .font(.headline)
+        }
+    }
+}
+
+struct ReflectionHistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
